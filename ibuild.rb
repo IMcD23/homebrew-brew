6ef04d78cbed3b65b0832580985f46cc10e06f97
Ian McDowell <me@ianmcdowell.net>
2018-02-07 15:53:06 -0800

    Added SHA1 for ibuild

--- ibuild.rb
+++ ibuild.rb
@@ -1,8 +1,8 @@
 class Ibuild < Formula
   desc "Build multi-architecture iOS libraries from build systems like CMake, Make, and Xcode."
   homepage "https://github.com/IMcD23/ibuild"
-  url "https://github.com/IMcD23/ibuild/archive/1.0.0.zip"
-  sha256 ""
+  url "https://github.com/IMcD23/ibuild/archive/1.0.0.tar.gz"
+  sha256 "2886df4f354afee6c8f79c279eeeccbba3ee6f4076202faec10b08e50d4e59ad"
   head "https://github.com/IMcD23/ibuild.git"
 
   depends_on :xcode
