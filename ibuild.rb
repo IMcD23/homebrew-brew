55b4fd1e4a5ff29c6547e789de6494e1cdc35849
Ian McDowell <me@ianmcdowell.net>
2018-02-07 16:55:45 -0800

    Updated to 1.0.1

--- ibuild.rb
+++ ibuild.rb
@@ -1,8 +1,8 @@
 class Ibuild < Formula
   desc "Build multi-architecture iOS libraries from build systems like CMake, Make, and Xcode."
   homepage "https://github.com/IMcD23/ibuild"
-  url "https://github.com/IMcD23/ibuild/archive/1.0.0.tar.gz"
-  sha256 "707fbe2c68e6aeb5acb940af81038146bb3ade9b39f407197dd34f6f06e11ba2"
+  url "https://github.com/IMcD23/ibuild/archive/1.0.1.tar.gz"
+  sha256 "350435ac0deca9b9fde5961c7f65a9dba5c476ea81305e6d82bab3be8b1a0537"
   head "https://github.com/IMcD23/ibuild.git"
 
   depends_on :xcode
